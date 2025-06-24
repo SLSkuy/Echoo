c48302fbe481645e336832e1d6a934e0e16436d2
2023051604013daibeini <2269595530@qq.com>
2025-06-24 09:22:57 +0800

    修改js文件

--- Client/listmodels.js
+++ Client/listmodels.js
@@ -3,10 +3,7 @@
 // var friendlistModel;
 var messagelistModel;
 
-var friendlistModel = Qt.createQmlObject(`
-    import QtQuick 2.15
-    ListModel {}
-`, Qt.application);
+var friendlistModel;
 
 
 function friendlistModelinit(listmodel){
@@ -17,7 +14,6 @@ function messagelistModelinit(listmodel){
     messagelistModel = listmodel
 }
 
-// 2. 添加好友到不同 Model 的方法
 function addFriend(user) {
     // 添加到好友列表
     friendlistModel.append({
@@ -29,7 +25,7 @@ function addFriend(user) {
 }
 
 function addMessagelist(user, message) {
-    // 添加到好友列表
+    // 添加到消息列表
     messagelistModel.append({
          picture: user.avatar,
         name: user.nickname,
@@ -50,20 +46,16 @@ function clearmessageModel(modelName) {
 }
 
 
-
-
-// 4. 暴露接口给 QML
-Qt.include({
-    // models: models,
-    addFriend: addFriend,
-    addMessagelist: addMessagelist,
-    clearfriendModel: clearfriendModel,
-    clearmessageModel: clearmessageModel,
-    friendlistModelinit: friendlistModelinit,
-    messagelistModelinit: messagelistModelinit,
-    friendlistModel: friendlistModel,
-               // models: models,
-    messagelistModel, messagelistModel
-});
+// Qt.include({
+//     // models: models,
+//     addFriend: addFriend,
+//     addMessagelist: addMessagelist,
+//     clearfriendModel: clearfriendModel,
+//     clearmessageModel: clearmessageModel,
+//     friendlistModelinit: friendlistModelinit,
+//     messagelistModelinit: messagelistModelinit,
+//     friendlistModel: friendlistModel,
+//     messagelistModel: messagelistModel
+// });
 
 
